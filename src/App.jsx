610d7967583d61f380646afb2592dea5c676a998
Vinayak Singh <vinayaksinghddn110@gmail.com>
2025-09-01 17:48:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import "./index.css"; // global css
@@ -8,16 +8,13 @@ function App() {
   return (
     <Router>
       <div>
-        {/* ✅ Navbar stays global */}
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-
         {/* ✅ Routes decide which page to render */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          {/* Default route -> About page */}
+          <Route path="/" element={<About />} />
+
+          {/* Home page */}
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </Router>
